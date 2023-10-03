c51cae47f33cbd54fe27a4d5a0f6440afdfc23dd
Yash Modi <ymodi@seattleu.edu>
2023-10-02 19:39:19 -0700

    Commit a change

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.Logging;
 
 using ContosoCrafts.WebSite.Models;
 using ContosoCrafts.WebSite.Services;
-
+// THis is a commit by Yash
 namespace ContosoCrafts.WebSite.Pages
 {
     /// <summary>
