789985acc83d623825b981ffa15b7209b2175159
Taekil Oh <toh3@seattleu.edu>
2023-10-02 19:39:15 -0700

    this merge testing taekil

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.Logging;
 
 using ContosoCrafts.WebSite.Models;
 using ContosoCrafts.WebSite.Services;
-
+// This comment is by Taekil
 namespace ContosoCrafts.WebSite.Pages
 {
     /// <summary>
