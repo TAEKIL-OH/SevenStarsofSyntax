c58a2084bf9170d02b9cf04b5728beef21461879
Yash Modi <ymodi@seattleu.edu>
2023-10-15 22:32:51 -0700

    Added a new model file PawModel.cs which does work like ProductModel.cs

---
+++ src/Models/PawModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+
+namespace ContosoCrafts.WebSite.Models
+{
+    public class Paw
+    {
+        public string Name { get; set; }
+        public string Breed { get; set; }
+        public string Gender { get; set; }
+        public string Age { get; set; }
+        public string Size { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+    }
+
+    public class Owner
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string City { get; set; }
+        public string Zipcode { get; set; }
+        public string Email { get; set; }
+    }
+    public class PawModel
+    {
+        public string Id { get; set; }
+        public Paw Paw { get; set; }
+        public Owner Owner { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<PawModel>(this);
+
+
+    }
+}
