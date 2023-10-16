d985261644411c5f013acaef87526ce0d60d6814
Yash Modi <ymodi@seattleu.edu>
2023-10-16 01:03:13 -0700

    Adding a new file JsonFilePawService.cs which works as JsonFileProductServices.cs

---
+++ src/Services/JsonFilePawService.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+
+using ContosoCrafts.WebSite.Models;
+using Microsoft.AspNetCore.Hosting;
+
+namespace ContosoCrafts.WebSite.Services
+{
+    public class JsonFilePawService
+    {
+        //Initializing the webHostEnvironment so it can help to find the location of the json file
+        public JsonFilePawService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        //Defining the path of the paws.json file
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "paws.json"); }
+        }
+
+        //This function will get the all paws data y opening the json file , deserialize it and read it to the end
+        public IEnumerable<PawModel> GetPaws()
+        {
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<PawModel[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+    }
+}
