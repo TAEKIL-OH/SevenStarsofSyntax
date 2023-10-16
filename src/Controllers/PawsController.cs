891360787afca074c9953a2184ca6df63a512845
Yash Modi <ymodi@seattleu.edu>
2023-10-16 01:17:08 -0700

    Add a new controller PawsController which will e used to perfrom CRUDi operations like ProductsController.cs

---
+++ src/Controllers/PawsController.cs
@@ -0,0 +1,23 @@
+﻿using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace ContosoCrafts.WebSite.Controllers
+{
+    public class PawsController : ControllerBase
+    {
+        public PawsController(JsonFilePawService pawService)
+        {
+            PawService = pawService;
+        }
+
+        public JsonFilePawService PawService { get; }
+
+        [HttpGet]
+        public IEnumerable<PawModel> Get()
+        {
+            return PawService.GetPaws();
+        }
+    }
+}
