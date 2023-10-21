8023a5937b460ad47dba33c5dce1f621898c4a33
Yash Modi <ymodi@seattleu.edu>
2023-10-21 14:55:22 -0700

    Added some validations in Update.cshtml.cs

--- src/Pages/Product/Update.cshtml.cs
+++ src/Pages/Product/Update.cshtml.cs
@@ -32,6 +32,127 @@ namespace ContosoCrafts.WebSite.Pages.Product
                 return Page();
             }
 
+            if(Paw.Paw.Name == null)
+            {
+                ModelState.AddModelError("Paw.Paw.Name", "Please donot keep this field empty");
+                return Page();
+            }
+            if (Paw.Paw.Name == "")
+            {
+                ModelState.AddModelError("Paw.Paw.Name", "Please donot keep this field empty");
+                return Page();
+            }
+            if (Paw.Paw.Breed == null)
+            {
+                ModelState.AddModelError("Paw.Paw.Breed", "Please donot keep this field empty");
+                return Page();
+            }
+            if (Paw.Paw.Breed == "")
+            {
+                ModelState.AddModelError("Paw.Paw.Breed", "Please donot keep this field empty");
+                return Page();
+            }
+            if (Paw.Paw.Age == null)
+            {
+                ModelState.AddModelError("Paw.Paw.Age", "Please donot keep this field empty");
+                return Page();
+            }
+            if (Paw.Paw.Age == "")
+            {
+                ModelState.AddModelError("Paw.Paw.Age", "Please donot keep this field empty");
+                return Page();
+            }
+            if (Paw.Paw.Size == null)
+            {
+                ModelState.AddModelError("Paw.Paw.Size", "Please donot keep this field empty");
+                return Page();
+            }
+            if (Paw.Paw.Size == "")
+            {
+                ModelState.AddModelError("Paw.Paw.Size", "Please donot keep this field empty");
+                return Page();
+            }
+            if (Paw.Paw.Description == null)
+            {
+                ModelState.AddModelError("Paw.Paw.Description", "Please donot keep this field empty");
+                return Page();
+            }
+            if (Paw.Paw.Description == "")
+            {
+                ModelState.AddModelError("Paw.Paw.Description", "Please donot keep this field empty");
+                return Page();
+            }
+            if (Paw.Paw.Image == null)
+            {
+                ModelState.AddModelError("Paw.Paw.Image", "Please donot keep this field empty");
+                return Page();
+            }
+            if (Paw.Paw.Image == "")
+            {
+                ModelState.AddModelError("Paw.Paw.Image", "Please donot keep this field empty");
+                return Page();
+            }
+            if (Paw.Owner.Name == null)
+            {
+                ModelState.AddModelError("Paw.Owner.Name", "Please donot keep this field empty");
+                return Page();
+            }
+            if (Paw.Owner.Name == "")
+            {
+                ModelState.AddModelError("Paw.Owner.Name", "Please donot keep this field empty");
+                return Page();
+            }
+            if (Paw.Owner.Address == null)
+            {
+                ModelState.AddModelError("Paw.Owner.Address", "Please donot keep this field empty");
+                return Page();
+            }
+            if (Paw.Owner.Address == "")
+            {
+                ModelState.AddModelError("Paw.Owner.Address", "Please donot keep this field empty");
+                return Page();
+            }
+            if (Paw.Owner.City == null)
+            {
+                ModelState.AddModelError("Paw.Owner.City", "Please donot keep this field empty");
+                return Page();
+            }
+            if (Paw.Owner.City == "")
+            {
+                ModelState.AddModelError("Paw.Owner.City", "Please donot keep this field empty");
+                return Page();
+            }
+            if (Paw.Owner.Zipcode == null)
+            {
+                ModelState.AddModelError("Paw.Owner.Zipcode", "Please donot keep this field empty");
+                return Page();
+            }
+            if (Paw.Owner.Zipcode == "")
+            {
+                ModelState.AddModelError("Paw.Owner.Zipcode", "Please donot keep this field empty");
+                return Page();
+            }
+            if (Paw.Owner.Email == null)
+            {
+                ModelState.AddModelError("Paw.Owner.Email", "Please donot keep this field empty");
+                return Page();
+            }
+            if (Paw.Owner.Email == "")
+            {
+                ModelState.AddModelError("Paw.Owner.Email", "Please donot keep this field empty");
+                return Page();
+            }
+            if (Paw.Owner.Phone == null)
+            {
+                ModelState.AddModelError("Paw.Owner.Phone", "Please donot keep this field empty");
+                return Page();
+            }
+            if (Paw.Owner.Phone == "")
+            {
+                ModelState.AddModelError("Paw.Owner.Phone", "Please donot keep this field empty");
+                return Page();
+            }
+
             PawService.UpdatePaw(Paw);
 
             return RedirectToPage("./Index");
