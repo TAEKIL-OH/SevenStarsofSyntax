6d3d4796dbd75694ddf3d85d42b84e9d98d5fcbf
zhouliu <zliu2@seattleu.edu>
2023-10-21 13:25:01 -0700

    Made the OnPost() function in Update.cshtml.cs

--- src/Pages/Product/Update.cshtml.cs
+++ src/Pages/Product/Update.cshtml.cs
@@ -1,3 +1,5 @@
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,32 @@ namespace ContosoCrafts.WebSite.Pages.Product
 {
     public class UpdateModel : PageModel
     {
+        public JsonFilePawService PawService { get; }
+
+        public UpdateModel(JsonFilePawService pawService)
+        {
+            PawService = pawService;
+        }
+
+
+
+        // The data to show
+        [BindProperty]
+        public PawModel Paw { get; set; }
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            PawService.UpdatePaw(Paw);
+
+            return RedirectToPage("./Index");
         }
     }
 }
