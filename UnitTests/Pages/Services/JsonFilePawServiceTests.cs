c65af7408a50d70d5d1c15e524e1a70c511f2578
zhouliu <zliu2@seattleu.edu>
2023-10-23 13:18:54 -0700

    Made the test region UpdatePaw in JsonFilePawServiceTests.cs which has the test cases to update the paw

--- UnitTests/Pages/Services/JsonFilePawServiceTests.cs
+++ UnitTests/Pages/Services/JsonFilePawServiceTests.cs
@@ -6,7 +6,7 @@ using NUnit.Framework;
 
 using ContosoCrafts.WebSite.Models;
 
-namespace UnitTests.Pages.Product.UpdatePaw
+namespace UnitTests.Pages.Product.AddRating
 {
     public class JsonFileProductServiceTests
     {
@@ -21,18 +21,73 @@ namespace UnitTests.Pages.Product.UpdatePaw
 
         #region UpdatePaw
         [Test]
-        public void UpdatePaw_Invalid_Paw_Null_Should_Return_Null()
+        public void UpdatePaw_Invalid_Paw_Id_Should_Return_Null()
         {
             //Arrange
-            var testpaw = new PawModel();
+            var testpaw = new PawModel
+            {
+                Id = "542526163512",
+                Paw = new Paw
+                {
+                    Name = "Paw",
+                    Breed = "Breed",
+                    Gender = "Gender",
+                    Age = "Age",
+                    Size = "Size",
+                    Description = "Description",
+                    Image = "Image"
+                },
+                Owner = new Owner
+                {
+                    Name = "Name",
+                    Address = "Address",
+                    City = "City",
+                    Zipcode = "Zipcode",
+                    Email = "paw@paw.com",
+                    Phone = "Phone"
+                }
+            };
 
             //Act
             var result = TestHelper.PawService.UpdatePaw(testpaw);
 
             //Assert
-            Assert.AreEqual(testpaw, result);
+            Assert.IsFalse(result);
+        }
+        [Test]
+        public void UpdatePaw_Valid_Paw_Id_Should_Return_True()
+        {
+            //Arrange
+            var testpaw = new PawModel
+            {
+                Id = "5425261635",
+                Paw = new Paw
+                {
+                    Name = "Paw",
+                    Breed = "Breed",
+                    Gender = "Gender",
+                    Age = "Age",
+                    Size = "Size",
+                    Description = "Description",
+                    Image = "Image"
+                },
+                Owner = new Owner
+                {
+                    Name = "Name",
+                    Address = "Address",
+                    City = "City",
+                    Zipcode = "Zipcode",
+                    Email = "paw@paw.com",
+                    Phone = "Phone"
+                }
+            };
+
+            //Act
+            var result = TestHelper.PawService.UpdatePaw(testpaw);
+
+            //Assert
+            Assert.IsTrue(result);
         }
         #endregion UpdatePaw
-
     }
 }
