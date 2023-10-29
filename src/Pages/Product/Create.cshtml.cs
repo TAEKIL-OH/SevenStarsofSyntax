9b36902c3f9c6ee7eaa58d7c8fc86b223be88711
Yash Modi <ymodi@seattleu.edu>
2023-10-29 14:41:00 -0700

    Initialized the pawservice and model in the Create.cshtml.cd so the Create.cshtml can use it.

--- src/Pages/Product/Create.cshtml.cs
+++ src/Pages/Product/Create.cshtml.cs
@@ -1,3 +1,5 @@
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,6 +7,17 @@ namespace ContosoCrafts.WebSite.Pages.Product
 {
     public class CreateModel : PageModel
     {
+        public JsonFilePawService PawService { get; }
+
+        public CreateModel(JsonFilePawService pawService)
+        {
+            PawService = pawService;
+        }
+
+        // The data to show
+        [BindProperty]
+        public PawModel Paw { get; set; }
+
         public void OnGet()
         {
         }
