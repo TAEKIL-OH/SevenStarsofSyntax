ae6e84ee762dc3d14934b015866b3142bd339ce7
Yash Modi <ymodi@seattleu.edu>
2023-10-30 22:07:01 -0700

    Added the appropriate comments in PawsController.cs

--- src/Controllers/PawsController.cs
+++ src/Controllers/PawsController.cs
@@ -5,18 +5,31 @@ using System.Collections.Generic;
 
 namespace ContosoCrafts.WebSite.Controllers
 {
+    /// <summary>
+    /// PawsController will enable calls to be made directly to controller
+    /// </summary>
     public class PawsController : ControllerBase
     {
+        /// <summary>
+        /// Constructor of PawsControlller 
+        /// </summary>
+        /// <param name="pawService"></param>
         public PawsController(JsonFilePawService pawService)
         {
             PawService = pawService;
         }
 
+        //Variale of PawService getter
         public JsonFilePawService PawService { get; }
 
+        /// <summary>
+        /// varaiable of IEnumerable of PawModel Type
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public IEnumerable<PawModel> Get()
         {
+            //Returned the list of all paws using the PawService method GetPaws()
             return PawService.GetPaws();
         }
     }
