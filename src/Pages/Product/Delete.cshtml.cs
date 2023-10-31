656b80978f9b70696e9b5a96bbebf4bb96c7e617
zhouliu <zliu2@seattleu.edu>
2023-10-31 13:35:01 -0700

    Applied the coding guidelines in Delete.cshtml.cs

--- src/Pages/Product/Delete.cshtml.cs
+++ src/Pages/Product/Delete.cshtml.cs
@@ -6,31 +6,51 @@ using System.Linq;
 
 namespace ContosoCrafts.WebSite.Pages.Product
 {
+    /// <summary>
+    /// This DeleteModel is the class for deleting the existing paw data
+    /// </summary>
     public class DeleteModel : PageModel
     {
-        public JsonFilePawService PawService { get; }
+        /// <summary>
+        /// Constructor of DeleteModel
+        /// </summary>
+        /// <param name="pawService"></param>
         public DeleteModel(JsonFilePawService pawService)
         {
             PawService = pawService;
         }
 
+        // Getter for PawServices
+        public JsonFilePawService PawService { get; }
 
 
-        // The data to show
+        // The data to show and delete
         [BindProperty]
         public PawModel Paw { get; set; }
+
+        /// <summary>
+        /// Rest OnGet method to display the data of selected paw 
+        /// </summary>
+        /// <param name="id"></param>
         public void OnGet(string id)
         {
             Paw = PawService.GetPaws().FirstOrDefault(m => m.Id.Equals(id));
         }
 
+        /// <summary>
+        /// REST OnPost method to delete the paw data
+        /// </summary>
+        /// <returns></returns>
         public IActionResult OnPost()
         {
+            // If model state is invalid then it will return to the page
             if (!ModelState.IsValid)
             {
                 ModelState.AddModelError("ModelOnly", "Something went wrong");
                 return Page();
             }
+
+            //If the paw dosent exists then return to the page with error
             var CheckPaw = PawService.GetPaws().FirstOrDefault(m => m.Id.Equals(Paw.Id));
             if (CheckPaw == null)
             {
@@ -38,6 +58,7 @@ namespace ContosoCrafts.WebSite.Pages.Product
                 return Page();
             }
 
+            //If the paw exists then it will delete that paw and redirect to index page
             PawService.DeletePaw(CheckPaw.Id);
             return RedirectToPage("./Index");
         }
