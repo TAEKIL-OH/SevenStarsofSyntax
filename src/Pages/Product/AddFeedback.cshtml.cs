22f5e62acfab14320c8f511a736df0b3b80e4712
zhouliu <zliu2@seattleu.edu>
2023-11-06 14:44:26 -0800

    Made the OnPost method in AddFeedback.cshtml.cs

--- src/Pages/Product/AddFeedback.cshtml.cs
+++ src/Pages/Product/AddFeedback.cshtml.cs
@@ -27,5 +27,30 @@ namespace ContosoCrafts.WebSite.Pages.Product
             Paw = PawService.GetPaws().FirstOrDefault(m => m.Id.Equals(id));
         }
 
+        public IActionResult OnPost(string message)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("ModelOnly", "Something went wrong");
+                return Page();
+            }
+
+            if (message == null)
+            {
+                ModelState.AddModelError("message", "Please donot keep this field empty");
+                return Page();
+            }
+
+            if (message == "")
+            {
+                ModelState.AddModelError("message", "Please donot keep this field empty");
+                return Page();
+            }
+
+            bool feedback = PawService.AddFeedckToPaw(Paw.Id, message);
+            return RedirectToPage("./Index");
+
+        }
+
     }
 }
