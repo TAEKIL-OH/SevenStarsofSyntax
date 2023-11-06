88af2a18539a14f2ade9e64d9138d790d7383faa
zhouliu <zliu2@seattleu.edu>
2023-11-06 14:37:19 -0800

    Initialized the paw service , model and OnGet Medthod in AddFeedback.cshtml.cs

--- src/Pages/Product/AddFeedback.cshtml.cs
+++ src/Pages/Product/AddFeedback.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,21 @@ namespace ContosoCrafts.WebSite.Pages.Product
 {
 	public class AddFeedbackModel : PageModel
     {
-        public void OnGet()
+        public AddFeedbackModel(JsonFilePawService pawService)
         {
+            PawService = pawService;
         }
+
+        public JsonFilePawService PawService { get; }
+
+        [BindProperty]
+        public PawModel Paw { get; set; }
+        public string message { get; set; }
+
+        public void OnGet(string id)
+        {
+            Paw = PawService.GetPaws().FirstOrDefault(m => m.Id.Equals(id));
+        }
+
     }
 }
