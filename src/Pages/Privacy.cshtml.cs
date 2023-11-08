2ab130607b8fd5d09010072f91168c2653782220
Yash Modi <ymodi@seattleu.edu>
2023-11-07 16:31:22 -0800

    Applied coding standards in Privacy.cshtml.cs

--- src/Pages/Privacy.cshtml.cs
+++ src/Pages/Privacy.cshtml.cs
@@ -3,17 +3,29 @@ using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+
+    /// <summary>
+    /// This model is made to display the privacy terms of pawfect
+    /// </summary>
     public class PrivacyModel : PageModel
     {
+
+        // Logger variable
         private readonly ILogger<PrivacyModel> _logger;
 
+        // Constructor that accepts logger as input
         public PrivacyModel(ILogger<PrivacyModel> logger)
         {
             _logger = logger;
         }
 
+        /// <summary>
+        /// In this page model OnGet will do nothing
+        /// </summary>
         public void OnGet()
         {
         }
+
     }
+
 }
