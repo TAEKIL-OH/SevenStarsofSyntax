707cfd25a5b0f3a92e0f5a3895f7592dd7c9f5f5
Yash Modi <ymodi@seattleu.edu>
2023-11-11 23:12:18 -0800

    Made the getter and setter of meetup model in MeetupModel.cs

--- src/Models/MeetupModel.cs
+++ src/Models/MeetupModel.cs
@@ -2,5 +2,10 @@
 {
     public class MeetupModel
     {
+        public string Id { get; set; } = System.Guid.NewGuid().ToString();
+        public string PawToMeet { get; set; }
+        public string DateOfMeetup { get; set; }
+        public string LocationOfMeetup { get; set; }
+        public string SpecialMessage { get; set; }
     }
 }
