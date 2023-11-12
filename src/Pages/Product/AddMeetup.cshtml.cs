efbfcd69bd10c18985502df19f24b8a9b13b9b1d
Taekil Oh <toh3@seattleu.edu>
2023-11-11 18:02:56 -0800

    Made the constructor of PawService in AddMeetup.cshtml.cs

--- src/Pages/Product/AddMeetup.cshtml.cs
+++ src/Pages/Product/AddMeetup.cshtml.cs
@@ -1,12 +1,31 @@
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
 
 namespace ContosoCrafts.WebSite.Pages.Product
 {
     public class AddMeetupModel : PageModel
     {
+        public AddMeetupModel(JsonFilePawService pawService)
+        {
+            PawService = pawService;
+        }
+
+        
+        public JsonFilePawService PawService { get; }
+
+        
+        [BindProperty]
+        public IEnumerable<PawModel> Paw { get; set; }
+        
+
         public void OnGet()
         {
+            Paw = PawService.GetPaws();
         }
+     
+
     }
 }
