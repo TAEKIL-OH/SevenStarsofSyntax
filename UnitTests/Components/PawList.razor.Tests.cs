e803aa68868985f9ab9862626be183dab465d0d7
Yash Modi <ymodi@seattleu.edu>
2023-11-14 13:27:41 -0800

    Added a new file PawList.razor.Tests.cs

---
+++ UnitTests/Components/PawList.razor.Tests.cs
@@ -0,0 +1,14 @@
+﻿using Bunit;
+using ContosoCrafts.WebSite.Components; // Adjust the namespace based on your project structure
+using ContosoCrafts.WebSite.Services;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace UnitTests.Components
+{
+    public class PawListTests
+    {
+    }
+}
