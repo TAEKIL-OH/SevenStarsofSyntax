5ae01accdd182819fcf8590468f7aaaaf5e0c653
Yash Modi <ymodi@seattleu.edu>
2023-11-19 14:36:21 -0800

    Made the gender enum attriutes in GenderTypeEnum.cs

--- src/Enums/GenderTypeEnum.cs
+++ src/Enums/GenderTypeEnum.cs
@@ -1,6 +1,11 @@
-﻿namespace ContosoCrafts.WebSite.Enums
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+namespace ContosoCrafts.WebSite.Enums
 {
-    public class GenderTypeEnum
+    public enum GenderTypeEnum
     {
+        [Display(Name = "Undefined")] Undefined = 0,
+        [Display(Name = "Male")] Male = 1,
+        [Display(Name = "Female")] Female = 2,
     }
 }
