452f36b4c35b74d8e626aaff9e21bf0ee0e39da3
Gayathri Gandham <ggandham@seattleu.edu>
2023-11-20 19:48:56 -0800

    Applied coding standards in MeetupModel.cs

--- src/Models/MeetupModel.cs
+++ src/Models/MeetupModel.cs
@@ -1,11 +1,23 @@
 ﻿namespace ContosoCrafts.WebSite.Models
 {
+    /// <summary>
+    /// Meetup Model Class
+    /// </summary>
     public class MeetupModel
     {
+        //Getter and Setter of Id
         public string Id { get; set; } = System.Guid.NewGuid().ToString();
+
+        //Getter and Setter of Paw To Meet
         public string PawToMeet { get; set; }
+
+        //Getter and Setter of Date of meetup
         public string DateOfMeetup { get; set; }
+
+        //Getter and Setter of Location of meetup
         public string LocationOfMeetup { get; set; }
+
+        //Getter and Setter of Special Message
         public string SpecialMessage { get; set; }
     }
 }
